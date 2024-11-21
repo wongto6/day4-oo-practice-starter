79b3bd0937b528287647281d3b36c0d443332ec3
WONGTO6 <wongto6@oocl.com>
2024-11-21 11:36:28 +0800

    fix: fix acceleration issue

--- src/afs/training/oo/Main.java
+++ src/afs/training/oo/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         GasolineEngine gasolineEngine = new GasolineEngine();
+        ElectricEngine electricEngine = new ElectricEngine();
 
-        Bus coolBus = new Bus("Cool Bus", 25, gasolineEngine);
+        Bus coolBus = new Bus("Cool Bus", 25, electricEngine);
         coolBus.speedUp();
 
         Truck bigTruck = new Truck("Big Truck", 20);
@@ -21,7 +22,6 @@ public class Main {
         Driver gasolineBusDriver = new Driver(gasolineBus);
         gasolineBusDriver.drive();
 
-        ElectricEngine electricEngine = new ElectricEngine();
         Bus electrirBus = new Bus("Electric Bus", 25, electricEngine);
         Driver electricBusDriver = new Driver(electrirBus);
         electricBusDriver.drive();
