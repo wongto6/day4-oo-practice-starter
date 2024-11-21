3830bb7177ca98d05c68dfe64240242c98cd0ceb
WONGTO6 <wongto6@oocl.com>
2024-11-21 11:20:57 +0800

    feat: finish ac2

--- src/afs/training/oo/Main.java
+++ src/afs/training/oo/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Bus coolBus = new Bus("Cool Bus", 25);
         coolBus.speedUp();
+
+        Truck bigTruck = new Truck("Big Truck", 20);
+        bigTruck.speedUp();
     }
 }
